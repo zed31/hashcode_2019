5a2749cbd61e780e53a8706250b852f64ed03740
Clement.Francois <clement.francois@kratosdefense.com>
2019-02-28 20:39:47 +0100

    Parse photo

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 import sys
+import data_model
 
 def print_credits():
 	print("This project has been made by a koala and an epitech student.")
 
 if __name__ == "__main__":
-	if len(sys.argv) == 2 and sys.argv[1] == "credits":
-		print_credits()
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "credits":
+			print_credits()
+		else:
+			photos = data_model.PhotoListModel(data_model.Parser(sys.argv[1]).get_photos())
+			print(photos.photos)
